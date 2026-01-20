eb40b6a7a31276d5ef24eb9e23c7be3909eb9f71
clkaha <claire.hagger@norfolk.gov.uk>
2026-01-20 12:56:12 +0000

    Changed today's weather heading to day/date/month.

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
 function getWeather(response) {
 	let temperatureTodayIcon = document.querySelector("#temperature-today-icon");
 	let temperatureTodayValue = document.querySelector(
-		"#temperature-today-value"
+		"#temperature-today-value",
 	);
 	let cityElement = document.querySelector("#city-name");
 	let todaysWeatherHeading = document.querySelector("#todays-weather-heading");
 	let nextSixDaysHeading = document.querySelector("#next-six-days-heading");
+	// let day2Heading = document.querySelector("#day2");
+	// let day3Heading = document.querySelector("#day3");
+	// let day4Heading = document.querySelector("#day4");
+	// let day5Heading = document.querySelector("#day5");
+	// let day6Heading = document.querySelector("#day6");
+	// let day7Heading = document.querySelector("#day7");
 	let liveTempTodayValue = Math.round(response.data.daily[0].temperature.day);
 	let liveTodayIconUrl = response.data.daily[0].condition.icon_url;
 	let cityName = response.data.city;
 	cityElement.innerHTML = cityName;
-	todaysWeatherHeading.innerHTML = `Today's weather forecast for ${cityName}`;
-	nextSixDaysHeading.innerHTML = `Weather forecast for the next six days in ${cityName}`;
+	todaysWeatherHeading.innerHTML = formatDate(new Date());
 	temperatureTodayValue.innerHTML = `${liveTempTodayValue}°C`;
 	temperatureTodayIcon.innerHTML = `<img src="${liveTodayIconUrl}" alt="">`;
+	nextSixDaysHeading.innerHTML = `Weather forecast for the next six days in ${cityName}`;
+	// day2Heading.innerHTML = `${days[1]}`;
+	// day3Heading.innerHTML = `${days[2]}`;
+	// day4Heading.innerHTML = `${days[3]}`;
+	// day5Heading.innerHTML = `${days[4]}`;
+	// day6Heading.innerHTML = `${days[5]}`;
+	// day7Heading.innerHTML = `${days[6]}`;
+}
+
+let now = new Date();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+let months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
+let date = now.getDate();
+let month = months[now.getMonth()];
+function formatDate() {
+	return `${day} ${date} ${month}`;
 }
 
 function searchCity(city) {
