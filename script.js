97dfdd4c630404f1a3fce4714e12f6c39fa3e2de
clkaha <claire.hagger@norfolk.gov.uk>
2026-01-20 14:49:03 +0000

    Added JS humidity and windspeed.

--- script.js
+++ script.js
@@ -5,27 +5,24 @@ function getWeather(response) {
 	);
 	let cityElement = document.querySelector("#city-name");
 	let todaysWeatherHeading = document.querySelector("#todays-weather-heading");
+	let descriptionToday = document.querySelector("#description-today");
+	let humidityToday = document.querySelector("#humidity-value");
 	let nextSixDaysHeading = document.querySelector("#next-six-days-heading");
-	// let day2Heading = document.querySelector("#day2");
-	// let day3Heading = document.querySelector("#day3");
-	// let day4Heading = document.querySelector("#day4");
-	// let day5Heading = document.querySelector("#day5");
-	// let day6Heading = document.querySelector("#day6");
-	// let day7Heading = document.querySelector("#day7");
+	let windspeedToday = document.querySelector("#windspeed");
+	let liveDescription = response.data.daily[0].condition.description;
+	let liveHumidityValue = response.data.daily[0].temperature.humidity;
+	let liveWindspeed = response.data.daily[0].wind.speed;
 	let liveTempTodayValue = Math.round(response.data.daily[0].temperature.day);
 	let liveTodayIconUrl = response.data.daily[0].condition.icon_url;
 	let cityName = response.data.city;
 	cityElement.innerHTML = cityName;
 	todaysWeatherHeading.innerHTML = formatDate(new Date());
+	descriptionToday.innerHTML = `${liveDescription}`;
+	humidityToday.innerHTML = `${liveHumidityValue}%`;
+	windspeedToday.innerHTML = `${liveWindspeed}km/h`;
 	temperatureTodayValue.innerHTML = `${liveTempTodayValue}°C`;
 	temperatureTodayIcon.innerHTML = `<img src="${liveTodayIconUrl}" alt="">`;
 	nextSixDaysHeading.innerHTML = `Weather forecast for the next six days in ${cityName}`;
-	// day2Heading.innerHTML = `${days[1]}`;
-	// day3Heading.innerHTML = `${days[2]}`;
-	// day4Heading.innerHTML = `${days[3]}`;
-	// day5Heading.innerHTML = `${days[4]}`;
-	// day6Heading.innerHTML = `${days[5]}`;
-	// day7Heading.innerHTML = `${days[6]}`;
 }
 
 let now = new Date();
